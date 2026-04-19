4fb50ab884bef21bbb492ccc05571f2863393c72
mnmonherdene1234 <mnmonherdene1234@gmail.com>
2026-04-20 00:14:54 +0800

    clean up

--- build-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-APP_NAME=filesgingo
-
-mkdir -p build
-
-GOOS=windows GOARCH=amd64 go build -o build/${APP_NAME}.exe .
-GOOS=linux GOARCH=amd64 go build -o build/${APP_NAME}-linux .
-GOOS=darwin GOARCH=amd64 go build -o build/${APP_NAME}-mac .
